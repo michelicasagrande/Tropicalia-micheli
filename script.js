f3e44cb542492c50cec23a618cba7600bdd803a8
michelicasagrande <160729387+michelicasagrande@users.noreply.github.com>
2025-03-26 14:26:02 +0000

    terminamos a aula 4

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
+    });
+    
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
