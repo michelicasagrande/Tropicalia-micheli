a6beea1e52de1ae05019f73a03e519ac00294fad
michelicasagrande <160729387+michelicasagrande@users.noreply.github.com>
2025-05-06 12:23:40 +0000

    cabou

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
 const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
     const alternaContraste = document.getElementById('alterna-contraste');
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 
 
     aumentaFonteBotao.addEventListener('click', function(){
@@ -30,3 +32,8 @@ const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
        })
 
 });
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
